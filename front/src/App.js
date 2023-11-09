a0b9d796fd5f6c6dac18b4af689c7747c66ffa52
Younext19 <haddamyounes5@gmail.com>
2023-11-09 22:31:05 +0100

    cfg : 🔧 add react router and auth pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Signup from "./pages/auth/signup/Signup";
+import { useState } from "react";
+import Feed from "./pages/feed";
+// import { atom, useAtom } from 'jotai'
 
 function App() {
+  const [hasToken, setHasToken] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>change</p>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {hasToken ? (
+          <Route path="/" element={<Feed />} />
+        ) : (
+          <Route path="signup" element={<Signup />} />
+        )}
+      </Routes>
     </div>
   );
 }
